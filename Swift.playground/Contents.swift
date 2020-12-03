018f0ddaf5e3736bf3bf4a1e85947d9984194e1f
Lian <lian@MacBook-Pro.local>
2020-12-03 13:19:39 +0800

    Part 1

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -6,8 +6,28 @@ import UIKit
 
 class Solution {
     
+    var x: Int = 0
+    
     func result(_ input: String) -> Int {
-        return 0
+        var count: Int = 0
+        
+        let rows = input.components(separatedBy: "\n")
+        guard let columns = rows.first?.count else {
+            fatalError("Input is invalid")
+        }
+        
+        for row in rows {
+            let c = row[row.index(row.startIndex, offsetBy: x)]
+            if c == "#" {
+                count += 1
+            }
+            x += 3
+            if x >= columns {
+                x -= columns
+            }
+        }
+        
+        return count
     }
 }
 
