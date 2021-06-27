3a7e394f2266d7725a25076443067d535868cc3b
Lian <lian@Mac-mini.local>
2021-06-27 18:37:08 +0800

    Day 7: Handy Haversacks

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -1,11 +1,27 @@
 import UIKit
 
 /*
- * 
+ * Day 7: Handy Haversacks
  */
 
 class Solution {
     
+    func result(for target: String, withInputRules inputRules: String) -> Int {
+        return 0
+    }
 }
 
 let sol = Solution()
+let input: String
+let target = "shiny gold"
+
+do {
+    guard let path = Bundle.main.path(forResource: "input", ofType: "txt") else {
+        fatalError("Path for input.txt is not found")
+    }
+    input = try String(contentsOfFile: path)
+} catch {
+    fatalError(error.localizedDescription)
+}
+
+print(sol.result(for: target, withInputRules: input))
