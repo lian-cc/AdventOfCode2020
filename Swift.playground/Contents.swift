4a7ab2ec3707e9a736a54df822d547b73f477ca7
Lian <lian@Mac-mini.local>
2021-07-01 21:09:28 +0800

    Part 2

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -19,36 +19,52 @@ class Solution {
     
     func result(for input: String) -> Int {
         let instructions: [Instruction] = input.components(separatedBy: CharacterSet.newlines).compactMap { Instruction(from: $0) }
-        let result = tryToGoal(for: instructions)
+        var changeIndex = -1
         
-        switch result {
-        case let .success(accumulator):
-            return accumulator
-        case .failure(.infiniteLoop(let accumulator)):
-            return accumulator
-        case .failure(.outOfRange):
-            fatalError("Out of range")
+        while changeIndex < instructions.count {
+            let result = tryToGoal(for: instructions, changeAfter: changeIndex)
+            switch result {
+            case let .success(accumulator):
+                return accumulator
+            case let .failure(.infiniteLoop(changedAtIndex)):
+                guard let index = changedAtIndex else {
+                    fatalError("No changes anymore")
+                }
+                changeIndex = index
+            case .failure(.outOfRange):
+                fatalError("Out of range")
+            }
         }
+        
+        fatalError("Answer is not found")
     }
     
     enum BootError: Error {
-        case infiniteLoop(afterAccumulator: Int)
+        case infiniteLoop(changedAt: Int?)
         case outOfRange
     }
     
-    func tryToGoal(for instructions: [Instruction]) -> Result<Int, BootError> {
+    func tryToGoal(for instructions: [Instruction], changeAfter: Int) -> Result<Int, BootError> {
         var runInstructions: [Instruction?] = instructions
         var index = 0
         var accumulator = 0
+        var runStep = 0
+        var changedAtStep: Int? = nil
         
-        while index < instructions.count {
+        while index >= 0, index < instructions.count {
             guard let instruction = runInstructions[index] else {
-                return Result.failure(.infiniteLoop(afterAccumulator: accumulator))
+                return Result.failure(.infiniteLoop(changedAt: changedAtStep))
             }
             
             runInstructions[index] = nil
             
             switch instruction.operation {
+            case .jumps where changedAtStep == nil && runStep > changeAfter:
+                changedAtStep = runStep
+                index += 1
+            case .noOperation where changedAtStep == nil && runStep > changeAfter:
+                changedAtStep = runStep
+                index += instruction.argument
             case .accumulator:
                 accumulator += instruction.argument
                 index += 1
@@ -57,6 +73,8 @@ class Solution {
             case .noOperation:
                 index += 1
             }
+            
+            runStep += 1
             
             if index == instructions.count {
                 return .success(accumulator)
