0b1c26012b35cb1128427f7b0aee49a1b039c67e
Lian <lian@Mac-mini.local>
2021-07-01 20:10:35 +0800

    Day 8: Handheld Halting

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -1,11 +1,26 @@
 import UIKit
 
 /*
- * 
+ * Day 8: Handheld Halting
  */
 
 class Solution {
     
+    func result(for input: String) -> Int {
+        return 0
+    }
 }
 
 let sol = Solution()
+let input: String
+
+do {
+    guard let path = Bundle.main.path(forResource: "input", ofType: "txt") else {
+        fatalError("Path for input.txt is not found")
+    }
+    input = try String(contentsOfFile: path)
+} catch {
+    fatalError(error.localizedDescription)
+}
+
+print(sol.result(for: input))
