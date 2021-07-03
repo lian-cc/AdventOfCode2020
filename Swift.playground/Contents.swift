c22c62aa5edff30f7eb0468dee0507ad1050dd33
Lian <lian@Mac-mini.local>
2021-07-03 15:09:39 +0800

    Part 1

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -7,7 +7,60 @@ import UIKit
 class Solution {
     
     func result(for input: String) -> Int {
-        return 0
+        let data = parseInputData(input).sorted()
+        return partOneAnswer(for: data)
+    }
+    
+    func partOneAnswer(for data: [Int]) -> Int {
+        let difference = countDifference(for: data)
+        guard
+            let diff1 = difference[1],
+            let diff3 = difference[3]
+        else {
+            fatalError("Count difference error")
+        }
+        return diff1 * diff3
+    }
+    
+    func countDifference(for data: [Int]) -> [Int: Int] {
+        guard !data.isEmpty else {
+            fatalError("Data is empty")
+        }
+        
+        var count = [
+            1: 0,
+            2: 0,
+            3: 0,
+        ]
+        
+        var data = data
+        var previousNumber = 0
+        
+        while !data.isEmpty {
+            let number = data.removeFirst()
+            let difference = number - previousNumber
+            
+            guard count.keys.contains(difference) else {
+                fatalError("Connection is broken")
+            }
+            
+            count[difference]! += 1
+            previousNumber = number
+        }
+        
+        count[3]! += 1
+        
+        return count
+    }
+    
+    func parseInputData(_ data: String) -> [Int] {
+        let components = data.components(separatedBy: CharacterSet.newlines).filter { $0.count > 0 }
+        return components.map { component in
+            guard let rowData = Int(component) else {
+                fatalError("Unknown data: \(component)")
+            }
+            return rowData
+        }
     }
 }
 
